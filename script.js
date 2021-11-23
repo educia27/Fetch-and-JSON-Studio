5a24b3dd3efff08afda1e5d534749960ac0234a3
educia27 <edd1114@gmail.com>
2021-11-22 20:06:29 -0600

    dynamically add list of astronauts, fetch, JSON, APIs

--- script.js
+++ script.js
@@ -84,45 +84,45 @@ window.addEventListener("load", function() {
          ]`
          let arrayOfObjects = JSON.parse(theJson);
          //console.log(arrayOfObjects);
-         arrayOfObjects.forEach(object => {
-            for (let key in object) {
-                console.log(`${key}: ${object[key]}`);
+         let item = ""
+         //arrayOfObjects.forEach(json => {
+            for (let values = 0; values < json.length; values++) {
+               // console.log(`${x}: ${object[values]}`);
+               item = `<div class="astronaut">
+               <div class="bio">
+               <h3>${json[values].firstName} ${json[values].lastName}</h3>
+               <ul>
+                   <li>Hours in space: ${json[values].hoursInSpace}</li>
+                   <li>Active: ${json[values].active}</li>
+                   <li>Skills: ${json[values].skills.join(", ")}</li>
+               </ul>
+               </div>
+               <img class="avatar" src= ${json[values].picture}>
+               </div>`
+               const contain = document.getElementById("container");
+               contain.innerHTML += item
+               if (json[values].active === true) {
+                   let color = document.getElementById("")
+               }
+
             }
-        });
-        const contain = document.getElementById("container");
-        contain.innerHTML  = `<div class="astronaut">
-            <div class="bio">
-            <h3>Name: ${json[0].firstName} ${json[0].lastName}</h3>
-            <ul>
-                <li>Hours in space: ${json[0].hoursInSpace}</li>
-                <li>Active: ${json[0].active}</li>
-                <li>Skills: ${json[0].skills}</li>
-            </ul>
-            </div>
-            <img class="avatar" src= ${json[0].picture}>
-            </div>
-            <div class="astronaut">
-            <div class="bio">
-            <h3>Name: ${json[1].firstName} ${json[1].lastName}</h3>
-            <ul>
-                <li>Hours in space: ${json[1].hoursInSpace}</li>
-                <li>Active: ${json[1].active}</li>
-                <li>Skills: ${json[1].skills}</li>
-            </ul>
-            </div>
-            <img class="avatar" src= ${json[1].picture}>
-            </div>
-            <div class="astronaut">
-            <div class="bio">
-            <h3>Name: ${json[2].firstName} ${json[2].lastName}</h3>
-            <ul>
-                <li>Hours in space: ${json[2].hoursInSpace}</li>
-                <li>Active: ${json[2].active}</li>
-                <li>Skills: ${json[2].skills}</li>
-            </ul>
-            </div>
-            <img class="avatar" src= ${json[2].picture}>
-            </div>`
+        //});
+        // const contain = document.getElementById("container");
+        // contain.innerHTML = item;
+
+
+
+        // contain.innerHTML  = `<div class="astronaut">
+        //     <div class="bio">
+        //     <h3>Name: ${json[0].firstName} ${json[0].lastName}</h3>
+        //     <ul>
+        //         <li>Hours in space: ${json[0].hoursInSpace}</li>
+        //         <li>Active: ${json[0].active}</li>
+        //         <li>Skills: ${json[0].skills}</li>
+        //     </ul>
+        //     </div>
+        //     <img class="avatar" src= ${json[0].picture}>
+        //     </div>`
        });
     });
  });
